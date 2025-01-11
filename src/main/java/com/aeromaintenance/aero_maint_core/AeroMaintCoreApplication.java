ebc48c336344dd96de789c7a728eb4b11b9a878a
aviation-web <aniket.shinde.dev01@gmail.com>
2025-01-11 10:19:31 +0530

    initial changes
    initial changes

--- src/main/java/com/aeromaintenance/aero_maint_core/AeroMaintCoreApplication.java
+++ src/main/java/com/aeromaintenance/aero_maint_core/AeroMaintCoreApplication.java
@@ -2,12 +2,18 @@ package com.aeromaintenance.aero_maint_core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+@EntityScan(basePackages = " com.aeromaintenance.product")
+@EnableJpaRepositories(basePackages = "com.aeromaintenance.product")
 
 @SpringBootApplication
 public class AeroMaintCoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AeroMaintCoreApplication.class, args);
+		System.out.println("Application Run..");
 	}
 
 }
