0a9706c6ff8084502c0e2b6a41b6b95b0131b735
aviation-web <aniket.shinde.dev01@gmail.com>
2025-02-08 12:05:29 +0530

    logincontroller

--- src/main/java/com/aeromaintenance/login/LoginController.java
+++ src/main/java/com/aeromaintenance/login/LoginController.java
@@ -9,24 +9,32 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:5173")
 public class LoginController {
 
-    @Autowired
-    private LoginService loginService;
-    public LoginController() {
-        System.out.println("LoginController Initialized");
-    }
-    // Register user
-    @PostMapping("/register")
-    public String register(@RequestBody Login user) {
-        loginService.registerUser(user);
-        return "User Registered Successfully!";
-    }
-
-    // Login user
-    @PostMapping("/login")
-    public String login(@RequestParam String username, @RequestParam String password) {
-        return loginService.validateLogin(username, password);
+//    @Autowired
+//    private LoginService loginService;
+//    public LoginController() {
+//        System.out.println("LoginController Initialized");
+//    }
+//    // Register user
+//    @PostMapping("/register")
+//    public String register(@RequestBody Login user) {
+//        loginService.registerUser(user);
+//        return "User Registered Successfully!";
+//    }
+//
+//    // Login user
+//    @PostMapping("/login")
+//    public String login(@RequestParam String username, @RequestParam String password) {
+//        return loginService.validateLogin(username, password);
+//    }
+	@PostMapping("/login")
+    public String validateLogin(@RequestBody LoginService request) {
+        if ("admin".equals(request.getUsername()) && "admin@123".equals(request.getPassword())) {
+            return "Login Successful!";
+        } else {
+            return "Login Failed!";
+        }
     }
 }
